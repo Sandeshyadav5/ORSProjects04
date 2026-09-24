3b8c975a068d88660f4263bdcded70eaf5be1dac
Durgesh Lohiya <157683259+Sandeshyadav5@users.noreply.github.com>
2026-09-24 14:59:45 +0530

    Refactor JDBCDataSource for improved clarity and structure

--- ORSProject04/src/main/java/com/sunilos/p4/util/JDBCDataSource.java
+++ ORSProject04/src/main/java/com/sunilos/p4/util/JDBCDataSource.java
@@ -1,3 +1,4 @@
+
 package com.sunilos.p4.util;
 
 import java.sql.Connection;
@@ -9,58 +10,92 @@ import com.sunilos.p4.exception.DatabaseException;
 
 /**
  * JDBC DataSource is a Data Connection Pool
- * 
- * @author  Durgesh Lohiya
- * @version 1.0
- * Copyright (c) Rays EdTech
+ *
+ * @author Durgesh Lohiya
+ * @version 1.0 Copyright (c) Rays EdTech
  */
- 
-public final class  JDBCDataSource {
+public final class JDBCDataSource {
 
 	/**
-	 * JDBC Database connection pool ( DCP )
+	 * JDBC Database connection pool (DCP)
 	 */
 	private static JDBCDataSource datasource;
-
-	private JDBCDataSource() {
-	}
 
 	private ComboPooledDataSource cpds = null;
 
 	/**
+	 * Private constructor
+	 */
+	private JDBCDataSource() {
+	}
+
+	/**
 	 * Create instance of Connection Pool
-	 * 
-	 * @return
+	 *
+	 * @return JDBCDataSource instance
 	 */
 	public static JDBCDataSource getInstance() {
+
 		if (datasource == null) {
 
 			ResourceBundle rb = ResourceBundle.getBundle("com.sunilos.p4.bundle.system");
 
 			datasource = new JDBCDataSource();
 			datasource.cpds = new ComboPooledDataSource();
-			try {
-				datasource.cpds.setDriverClass(rb.getString("driver"));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			datasource.cpds.setJdbcUrl(rb.getString("url"));
-			datasource.cpds.setUser(rb.getString("username"));
-			datasource.cpds.setPassword(rb.getString("password"));
-			datasource.cpds.setInitialPoolSize(DataUtility.getInt((rb.getString("initialPoolSize"))));
-			datasource.cpds.setAcquireIncrement(DataUtility.getInt(rb.getString("acquireIncrement")));
-			datasource.cpds.setMaxPoolSize(DataUtility.getInt(rb.getString("maxPoolSize")));
-			datasource.cpds.setMaxIdleTime(DataUtility.getInt(rb.getString("timeout")));
-			datasource.cpds.setMinPoolSize(DataUtility.getInt(rb.getString("minPoolSize")));
 
+			try {
+
+				// Set JDBC Driver
+				datasource.cpds.setDriverClass(rb.getString("driver"));
+
+				/*
+				 * Database URL
+				 *
+				 * Docker: DATABASE_URL=jdbc:mysql://mysql:3306/project04
+				 *
+				 * Local: jdbc:mysql://localhost:3306/project04
+				 */
+				String dbUrl = System.getenv("DATABASE_URL");
+
+				if (dbUrl == null || dbUrl.trim().isEmpty()) {
+					dbUrl = rb.getString("url.local");
+				}
+
+				datasource.cpds.setJdbcUrl(dbUrl);
+
+				// Database username
+				datasource.cpds.setUser(rb.getString("username"));
+
+				// Database password
+				datasource.cpds.setPassword(rb.getString("password"));
+
+				// Connection Pool Configuration
+				datasource.cpds.setInitialPoolSize(DataUtility.getInt(rb.getString("initialPoolSize")));
+
+				datasource.cpds.setAcquireIncrement(DataUtility.getInt(rb.getString("acquireIncrement")));
+
+				datasource.cpds.setMaxPoolSize(DataUtility.getInt(rb.getString("maxPoolSize")));
+
+				datasource.cpds.setMaxIdleTime(DataUtility.getInt(rb.getString("timeout")));
+
+				datasource.cpds.setMinPoolSize(DataUtility.getInt(rb.getString("minPoolSize")));
+
+			} catch (Exception e) {
+
+				e.printStackTrace();
+
+				throw new DatabaseException("Database connection pool initialization failed: " + e.getMessage());
+			}
 		}
+
 		return datasource;
 	}
 
 	/**
 	 * Gets the connection from ComboPooledDataSource
-	 * 
+	 *
 	 * @return connection
+	 * @throws SQLException
 	 */
 	public static Connection getConnection() throws SQLException {
 		return getInstance().cpds.getConnection();
@@ -68,29 +103,41 @@ public final class  JDBCDataSource {
 
 	/**
 	 * Closes a connection
-	 * 
-	 * @param connection
-	 
+	 *
+	 * @param connection connection object
 	 */
 	public static void closeConnection(Connection connection) {
+
 		if (connection != null) {
+
 			try {
 				connection.close();
+
 			} catch (SQLException e) {
-				throw new DatabaseException("Collection close exception" + e.getMessage());
+
+				throw new DatabaseException("Connection close exception " + e.getMessage());
 			}
 		}
 	}
 
+	/**
+	 * Rollback transaction
+	 *
+	 * @param connection connection object
+	 */
 	public static void rollBack(Connection connection) {
+
 		if (connection != null) {
+
 			try {
 				connection.rollback();
+
 			} catch (SQLException e) {
+
 				e.printStackTrace();
-				throw new DatabaseException("Rollback exception" + e.getMessage());
+
+				throw new DatabaseException("Rollback exception " + e.getMessage());
 			}
 		}
 	}
-
 }
